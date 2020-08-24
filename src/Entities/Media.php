4ca2ed1d5dd5c38f6a85e310ddc1daf489a447e3
Greg Mayes <gregmayes@langleyfoxall.co.uk>
2020-08-24 15:16:49 +0100

    Added property

--- src/Entities/Media.php
+++ src/Entities/Media.php
@@ -6,6 +6,7 @@ namespace Optimus\Entities;
  * @see Product
  * @property-read string $thumbnail
  * @property-read string $url
+ * @property-read int $updated_at Unix timestamp
  */
 class Media extends AbstractEntity
 {
